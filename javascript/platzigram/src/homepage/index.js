556f7b0b50382b57652466d7f731205479a099c6
Gerardo Lopez <mcgalv@gmail.com>
2017-03-24 17:44:07 -0600

    Navbar Template

--- javascript/platzigram/src/homepage/index.js
+++ javascript/platzigram/src/homepage/index.js
@@ -1,11 +1,16 @@
+
 // Modularizando el proyecto, cada modulo se crea en una carpeta diferente dentro del src
 // Se deben incluir las librerias necesarias para utilizarlo.
 var page = require("page");
 var empty = require('empty-element');
+
+// ya que tenemos nuestro template lo podemos requerir en nuestra logica de rutas
+// no es necesario indicar la extension js.
+var template = require("./template");
 var title = require('title');
-// Se crea la ruta para esta pagina posteriormente se incluyen en el index principal
-page("/", function(ctx, f){
-	title('Platzigram');
-	var main = document.getElementById("main-container");
-	empty(main).innerHTML = "<a href='/signup'>Signup</a>";
+
+page("/", function(ctx, f){  
+  title('Platzigram');
+  var main = document.getElementById("main-container");
+  empty(main).appendChild(template);	
 });
---
+++ javascript/platzigram/src/homepage/template.js
@@ -0,0 +1,31 @@
+// Separamos la vista yo-yo de la logica de la ruta
+var yo = require('yo-yo');
+// var landing = require("../landing") // toma la funcion devuelta por ese archivo
+// para poder incluir esta vista en otro archivo debe de usarse module.exports
+// la cual es una variable disponible en javascript la cual nos devuelve un elemento.
+// del DOM.
+
+var template = yo`<nav class="header">
+    <div class="nav-wrapper">
+      <div class="container">
+        <div class="row">
+          <div class="col s12 m6 offset-m1">
+            <a href="/" class="brand-logo platzigram">Platzigram</a>
+          </div>
+          <div class="col s2 m6 push-m10">
+            <a href="" class="btn btn-large btn-flat dropdown-button" data-activates="drop-user">
+              <i class="fa fa-user"></i>
+            </a>
+            <ul id="drop-user" class="dropdown-content">
+              <li><a href="#">Salir</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>`;
+
+// El template debe devolver una vista la cual logramos construir al incluir 
+// landing como funcion y llamarla dentro de nuestra vista pero mandando el 
+// box que la va a complementar. (Formulario);
+module.exports = template;
